99e14da6cfcdc64b1253f95778d9aab16bd7d4a4
Pradeep Naraharirao <pradeepnaraharirao@Pradeeps-MacBook-Pro.local>
2020-04-02 16:57:16 -0700

    Add another dimension

--- nbit_sr.sv
+++ nbit_sr.sv
@@ -1,22 +1,23 @@
 module nbit_sr
     #(parameter DSIZE  = 1,
       parameter DLY    = 1,
-      parameter SH_R   = 1)
+      parameter SH_R   = 1,
+      parameter VEC    = 10)
     (
-     input  logic [DSIZE-1:0]  din,
-     output logic [DSIZE-1:0]  dout,
+     input  logic [DSIZE-1:0]  din   [VEC],
+     output logic [DSIZE-1:0]  dout  [VEC],
      input  logic              rst_b,
      input  logic              clk
      );
 
-logic [DSIZE-1:0] stg [0:DLY-1];
+logic [DSIZE-1:0] stg [0:DLY-1][VEC];
 
 
 if(SH_R == 1)
  always@(posedge clk or negedge rst_b) 
   stg<=(~rst_b)?'0:{din,stg[DLY-1:1]};
 else
- always@(posedge clk or negedge rst_b) begin
+ always@(posedge clk or negedge rst_b)
   stg<=(~rst_b)?'0:{stg[DLY-2:0],din};
 
 
